bd90a07f55fee34f4eeff46c95fc2bd59af9c7b1
ghabxph <11975597+ghabxph@users.noreply.github.com>
2024-04-29 12:48:53 +0800

    feat: moved client outside src folder

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,9 @@
-import { generate } from "@devexperts/swagger-codegen-ts";
-import { OpenapiObjectCodec } from "@devexperts/swagger-codegen-ts/dist/schema/3.0/openapi-object";
-import { serialize } from "@devexperts/swagger-codegen-ts/dist/language/typescript/3.0";
-import path from "path";
 
-const spec = path.resolve(__dirname, "../swagger.json");
-const out = path.resolve(__dirname, "src/generated");
-const result = generate({
-  spec: path.resolve(__dirname, "../swagger.json"),
-  out: path.resolve(__dirname, "src/generated"),
-  language: serialize,
-  decoder: OpenapiObjectCodec,
-});
+import * as client from "../swagger-client";
 
-console.log(spec);
-console.log(out);
-result().then(v => {
-  console.log(v);
-}).catch(e => {
-  console.error(e);
-});
+
+async function main() {
+  client.GeneralEndpointsApiAxiosParamCreator(new client.Configuration()).poolsGet();
+}
+
+main();
