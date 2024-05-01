2ee91d26f53247e769cc6c4a4cbe35a83f6cc140
ghabxph <11975597+ghabxph@users.noreply.github.com>
2024-05-01 10:45:20 +0800

    test: wip

--- hawk-api/src/index.ts
+++ hawk-api/src/index.ts
@@ -1,9 +1,214 @@
+import * as client from "../../swagger-client";
+import { HealthResponse, ResponseWithStatus, UserPortfolioOut } from "./types";
 
-import * as client from "../swagger-client";
-
-
-async function main() {
-  client.GeneralEndpointsApiAxiosParamCreator(new client.Configuration()).poolsGet();
+class Client {
+  public readonly config: client.Configuration;
+  public readonly healthCheck: client.HealthCheckApi;
+  public readonly generalEndpoints: client.GeneralEndpointsApi;
+  public readonly meteoraDLMMUtilityFunctionsApi: client.MeteoraDLMMUtilityFunctionsApi;
+  public readonly meteoraDLMMInstructionsApi: client.MeteoraDLMMInstructionsApi;
+  public readonly meteoraDLMMAutomationInstructionsApi: client.MeteoraDLMMAutomationInstructionsApi;
+  public readonly orcaUtilityFunctionsApi: client.OrcaUtilityFunctionsApi;
+  public readonly orcaCLMMInstructionsApi: client.OrcaCLMMInstructionsApi;
+  constructor(
+    public readonly url: string = "https://api2.hawksight.co",
+  ) {
+    this.config = new client.Configuration({
+      basePath: url
+    });
+    this.healthCheck = new client.HealthCheckApi(this.config);
+    this.generalEndpoints = new client.GeneralEndpointsApi(this.config);
+    this.meteoraDLMMUtilityFunctionsApi = new client.MeteoraDLMMUtilityFunctionsApi(this.config);
+    this.meteoraDLMMInstructionsApi = new client.MeteoraDLMMInstructionsApi(this.config);
+    this.meteoraDLMMAutomationInstructionsApi = new client.MeteoraDLMMAutomationInstructionsApi(this.config);
+    this.orcaUtilityFunctionsApi = new client.OrcaUtilityFunctionsApi(this.config);
+    this.orcaCLMMInstructionsApi = new client.OrcaCLMMInstructionsApi(this.config);
+  }
 }
 
-main();
+class Health {
+  constructor(
+    private readonly client: Client,
+  ) {}
+  async health(): Promise<ResponseWithStatus<HealthResponse>> {
+    const result = await this.client.healthCheck.healthGet().catch(e => e.response);
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+}
+
+class General {
+  constructor(
+    private readonly client: Client,
+  ) {}
+
+  async portfolio(
+    params: {
+      wallet: string,
+      pool?: string,
+    }
+  ): Promise<ResponseWithStatus<UserPortfolioOut>> {
+    const result = await this.client.generalEndpoints.portfolioGet(params.wallet, params.pool).catch(e => e.response);
+    return {
+      status: result.status,
+      data: result.data,
+    }
+  }
+  async pools(): Promise<ResponseWithStatus<client.InlineResponse2002[]>> {
+    const result = await this.client.generalEndpoints.poolsGet();
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+
+  async tokens(): Promise<ResponseWithStatus<client.InlineResponse2003[]>> {
+    const result = await this.client.generalEndpoints.tokensGet();
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+
+  async register(params: client.RegisterBody): Promise<ResponseWithStatus<client.InlineResponse2004>> {
+    const result = await this.client.generalEndpoints.registerPost(params);
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+}
+
+class Util {
+  constructor(
+    private readonly client: Client,
+  ) {}
+
+  async meteoraDlmmPools(): Promise<ResponseWithStatus<client.InlineResponse2005[]>> {
+    const result = await this.client.meteoraDLMMUtilityFunctionsApi.meteoraDlmmUtilPoolsGet();
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+
+  async meteoraDlmmPositions(
+    params: {
+      wallet: string,
+      pool?: string,
+    }
+  ): Promise<ResponseWithStatus<any>> {
+    const result = await this.client.meteoraDLMMUtilityFunctionsApi.meteoraDlmmUtilPositionsGet(params.wallet, params.pool);
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+
+  async meteoraDlmmActiveBin(params: client.UtilActiveBinBody): Promise<ResponseWithStatus<any>> {
+    const result = await this.client.meteoraDLMMUtilityFunctionsApi.meteoraDlmmUtilActiveBinPost(params);
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+
+  async orcaClmmPools(): Promise<ResponseWithStatus<any>> {
+    return await this.client.orcaUtilityFunctionsApi.orcaUtilPoolsGet();
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+
+  async orcaPositions(
+    params: {
+      wallet: string,
+      pool?: string,
+    }
+  ): Promise<ResponseWithStatus<any>> {
+    return await this.client.orcaUtilityFunctionsApi.orcaUtilPositionsGet(params.wallet, params.pool);
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+
+  async orcaGetPositionMint(
+    params: {
+      position: string,
+    }
+  ): Promise<ResponseWithStatus<any>> {
+    return await this.client.orcaUtilityFunctionsApi.orcaUtilGetPositionMintGet(params.position);
+    return {
+      status: result.status,
+      data: result.data,
+    };
+  }
+}
+
+class TxGenerator {
+  constructor(
+    private readonly client: Client,
+  ) {}
+
+  async meteoraCreatePositionAndDeposit(params: client.TxCreatePositionAndDepositBody): Promise<ResponseWithStatus<any>> {
+    return await this.client.meteoraDLMMInstructionsApi.meteoraDlmmTxCreatePositionAndDepositPost(params);
+  }
+
+  async meteoraDeposit(params: client.TxDepositBody): Promise<ResponseWithStatus<any>> {
+    return await this.client.meteoraDLMMInstructionsApi.meteoraDlmmTxDepositPost(params);
+  }
+
+  async meteoraWithdraw(params: client.TxWithdrawBody): Promise<ResponseWithStatus<any>> {
+    return await this.client.meteoraDLMMInstructionsApi.meteoraDlmmTxWithdrawPost(params);
+  }
+
+  async meteoraClaim(params: client.TxClaimBody): Promise<ResponseWithStatus<any>> {
+    return await this.client.meteoraDLMMInstructionsApi.meteoraDlmmTxClaimPost(params);
+  }
+
+  async meteoraClosePosition(params: client.TxClaimBody): Promise<ResponseWithStatus<any>> {
+    return await this.client.meteoraDLMMInstructionsApi.meteoraDlmmTxClosePositionPost(params);
+  }
+
+  async orcaOpenPosition(params: client.TxOpenPositionBody): Promise<ResponseWithStatus<any>> {
+    return await this.client.orcaCLMMInstructionsApi.orcaTxOpenPositionPost(params);
+  }
+
+  async orcaClosePosition(params: client.TxClosePositionBody1): Promise<ResponseWithStatus<any>> {
+    return await this.client.orcaCLMMInstructionsApi.orcaTxClosePositionPost(params);
+  }
+
+  async orcaDeposit(params: client.TxDepositBody1): Promise<ResponseWithStatus<any>> {
+    return await this.client.orcaCLMMInstructionsApi.orcaTxDepositPost(params);
+  }
+
+  async orcaWithdraw(params: client.TxWithdrawBody1): Promise<ResponseWithStatus<any>> {
+    return await this.client.orcaCLMMInstructionsApi.orcaTxWithdrawPost(params);
+  }
+
+  async orcaClaimRewards(params: client.TxClaimRewardsBody): Promise<ResponseWithStatus<any>> {
+    return await this.client.orcaCLMMInstructionsApi.orcaTxClaimRewardsPost(params);
+  }
+}
+
+class HawkAPI {
+  public readonly health: Health;
+  public readonly general: General;
+  public readonly util: Util;
+  public readonly txGenerator: TxGenerator;
+  constructor(
+    protected readonly url: string = "https://api2.hawksight.co",
+  ) {
+    const client = new Client(url);
+    this.health = new Health(client);
+    this.general = new General(client);
+    this.util = new Util(client);
+    this.txGenerator = new TxGenerator(client);
+  }
+}
+
+export default HawkAPI;
---
+++ hawk-api/src/types.ts
@@ -0,0 +1,240 @@
+import * as web3 from "@solana/web3.js";
+import BN from "bn.js";
+
+export type AccountMeta = {
+  isSigner: boolean,
+  isWritable: boolean,
+  pubkey: string,
+}
+
+export type TransactionInstruction = {
+  programId: string,
+  keys: AccountMeta[],
+  data: string
+};
+
+export type Transaction = TransactionInstruction[];
+
+export type Web3Transaction = web3.TransactionInstruction[];
+
+export type BinRange = {
+  lowerRange: number,
+  upperRange: number,
+}
+
+export type Distribution = "SPOT" | "CURVE" | "BID-ASK";
+
+export type MeteoraCreatePositionAndDeposit = {
+  position: web3.PublicKey,
+  pool: string,
+  userWallet: web3.PublicKey,
+  totalXAmount: BN,
+  totalYAmount: BN,
+  binRange: BinRange,
+  distribution: Distribution,
+};
+
+export type MeteoraDeposit = {
+  position: web3.PublicKey,
+  pool: string,
+  userWallet: web3.PublicKey,
+  totalXAmount: BN,
+  totalYAmount: BN,
+  distribution: Distribution,
+};
+
+export type MeteoraPoolInfo = {
+  address: string,
+  name: string,
+  mint_x: string,
+  mint_y: string,
+  reserve_x: string,
+  reserve_y: string,
+  reserve_x_amount: number,
+  reserve_y_amount: number,
+  bin_step: number,
+  base_fee_percentage: string,
+  max_fee_percentage: string,
+  protocol_fee_percentage: string,
+  liquidity: string,
+  reward_mint_x: string,
+  reward_mint_y: string,
+  fees_24h: number,
+  today_fees: number,
+  trade_volume_24h: number,
+  cumulative_trade_volume: string,
+  cumulative_fee_volume: string,
+  current_price: number,
+  apr: number,
+  apy: number,
+  hide: boolean
+}
+
+export enum Tags {
+  Stable = "stable",
+  Volatile = "volatile",
+  LiquidStaking = "liquid_staking",
+  Stablecoin = "stablecoins",
+  Deprecated = "deprecated",
+}
+
+export enum Protocol {
+  Saber = "saber",
+  Orca = "orca",
+  Meteora = "meteora",
+}
+
+interface BasePool {
+  id: string;
+  name: string;
+  url: string;
+  tags: Tags[];
+  hidden: boolean;
+}
+
+export type Pool =
+  | (BasePool & {
+      protocol: Protocol.Saber;
+      config: SaberPoolConfig; // contains all the related accounts to this strategy
+    })
+  | (BasePool & {
+      protocol: Protocol.Orca;
+      config: OrcaPoolConfig; // contains all the related accounts to this strategy
+    })
+  | (BasePool & {
+      protocol: Protocol.Meteora;
+      config: MeteoraPoolConfig; // contains all the related accounts to this strategy
+    });
+
+export type SaberPoolConfig = {};
+export type OrcaPoolConfig = OrcaPoolInfo;
+export type MeteoraPoolConfig = {
+  address: string,
+  name: string,
+  mint_x: string,
+  mint_y: string,
+  reserve_x: string,
+  reserve_y: string,
+  reward_mint_x: string,
+  reward_mint_y: string,
+  bin_step: number,
+};
+
+export type Token = {
+  address: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  logo: string;
+};
+
+// This should be serializable to web3.TransactionInstruction
+export type Instruction = {
+  accounts: {
+    isSigner: boolean;
+    isWritable: boolean;
+    pubkey: string;
+  }[];
+  data: string;
+  programId: string;
+};
+
+export type TransactionMetadata = {
+  description: string;
+  estimatedFeeInSOL: string,
+  addressLookupTableAddresses: string[]; // "alts" is confusing, copied jup's naming
+  computeBudgetInstructions: Instruction[]; // this enables for ease of access while also making them optional
+  mainInstructions: Instruction[];
+}
+
+export type TransactionPriority = "Default" | "Low" | "Medium" | "High" | "VeryHigh" | "UnsafeMax" | "None";
+
+export type Balance = {
+  amount: string;
+  mint: string;
+};
+
+export type UserPositionBalances = {
+  balances: Balance[],
+  fees: Balance[],
+  rewards: Balance[],
+};
+
+export type PoolOut = UserPositionBalances & {
+  positionAddress: string,
+}
+
+export type UserPortfolioOut = {
+  wallet: string,
+  userPda: string,
+  pools: Record<string, PoolOut[]>,
+}
+
+export type OrcaPoolInfo = {
+  address: string,
+  feeApr: {
+    day: number,
+    month: number,
+    week: number,
+  }
+  lpFeeRate: number,
+  modifiedTimeMs: number,
+  price: number,
+  priceRange: {
+    day: { min: number, max: number },
+    month: { min: number, max: number },
+    week: { min: number, max: number },
+  },
+  protocolFeeRate: number,
+  reward0Apr: { day: number, week: number, month: number },
+  reward1Apr: { day: number, week: number, month: number },
+  reward2Apr: { day: number, week: number, month: number },
+  tickSpacing: number,
+  tokenA: {
+    coingeckoId: string,
+    decimals: number,
+    logoURI: string,
+    mint: string,
+    name: string,
+    poolToken: boolean,
+    symbol: string,
+    whitelisted: boolean,
+  }
+  tokenB: {
+    coingeckoId: string,
+    decimals: number,
+    logoURI: string,
+    mint: string,
+    name: string,
+    poolToken: boolean,
+    symbol: string,
+    whitelisted: boolean,
+  },
+  totalApr: { day: number, week: number, month: number },
+  tvl: number,
+  volume: { day: number, week: number, month: number },
+  volumeDenominatedA: { day: number, week: number, month: number },
+  volumeDemonimatedB: { day: number, week: number, month: number },
+  whirlpoolsConfig: string,
+  whitelisted: boolean,
+}
+
+export type OrcaPoolResponse = {
+  whirlpools: OrcaPoolInfo[],
+};
+
+export type OrcaPoolInfoIndexed = Record<string, OrcaPoolInfo>;
+
+export type TokenAccountInfo = {
+  address: web3.PublicKey,
+  owner: web3.PublicKey,
+  mint: web3.PublicKey,
+  amount: BN,
+}
+
+export type ResponseWithStatus<T> = {
+  status: number,
+  data: T
+}
+
+export type HealthResponse = Record<string, "OK" | "NOT OK">;
