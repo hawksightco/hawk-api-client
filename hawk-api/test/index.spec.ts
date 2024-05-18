6b1eb3ae2f18f56243f07e12b873ab5c4058ca7e
ghabxph <11975597+ghabxph@users.noreply.github.com>
2024-05-18 17:07:01 +0800

    test(hawk-api): updated test position

--- hawk-api/test/index.spec.ts
+++ hawk-api/test/index.spec.ts
@@ -8,7 +8,7 @@ const testWallet = 'Ga5jNBh26JHh9zyJcdm7vpyVWRgtKS2cLpNgEc5zBv8G';
 const hawkWallet = 'dche7M2764e8AxNihBdn7uffVzZvTBNeL8x4LZg5E2c';
 const connection = new web3.Connection('https://mainnet-beta.solana.com'); // change this to private rpc
 const testPool = 'ARwi1S4DaiTG5DX7S4M4ZsrXqpMD1MrTmbu9ue2tpmEq';
-const testPosition = 'EFET5gK1FJnDovjHpG8yScp1NLTc6pPjUtxWUeAUx3i1';
+const testPosition = '3xbDyEuRKWRtQSoHV2R7fqQpGS1SKacL1gQp1vGzaYgp';
 let activeBin: number;
 
 const jestConsole = console;
