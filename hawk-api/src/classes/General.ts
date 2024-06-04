2e731339b41c752d4ebd3bc510dab58fc67f9a2c
ghabxph <11975597+ghabxph@users.noreply.github.com>
2024-06-04 15:53:22 +0800

    feat(hawk-api): bumped to 0.0.34: added verified flag and added eugene's PR

--- hawk-api/src/classes/General.ts
+++ hawk-api/src/classes/General.ts
@@ -87,7 +87,7 @@ export class General {
    *
    * @returns A Promise resolving to an array of token details, including names, symbols, and other token-specific information.
    */
-  async tokens(): Promise<ResponseWithStatus<_client.InlineResponse2002[]>> {
+  async tokens(): Promise<ResponseWithStatus<_client.InlineResponse2001[]>> {
     const result = await this.client.generalEndpoints.tokensGet().catch(e => e.response);
     return {
       status: result.status,
