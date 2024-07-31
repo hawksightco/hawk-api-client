fea80427457b7a154778a32a0f68a154c685db72
ツカサちゃん <11975597+ghabxph@users.noreply.github.com>
2024-07-31 12:37:54 +0800

    feat(swagger-client): bump to 0.0.28: added bin init ix

--- swagger-client/apis/meteora-dlmmautomation-instructions-api.ts
+++ swagger-client/apis/meteora-dlmmautomation-instructions-api.ts
@@ -20,7 +20,6 @@ import { BASE_PATH, COLLECTION_FORMATS, RequestArgs, BaseAPI, RequiredError } fr
 import { AutomationCompoundAutomationIxBody } from '../models';
 import { AutomationLimitCloseAutomationIxBody } from '../models';
 import { AutomationRebalanceAutomationIxBody } from '../models';
-import { AutomationWithdrawAndCloseAutomationIxBody } from '../models';
 import { InlineResponse400 } from '../models';
 import { TransactionMetadata } from '../models';
 /**
@@ -155,48 +154,6 @@ export const MeteoraDLMMAutomationInstructionsApiAxiosParamCreator = function (c
                 options: localVarRequestOptions,
             };
         },
-        /**
-         * Performs a full withdrawal and closes position
-         * @param {AutomationWithdrawAndCloseAutomationIxBody} body 
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        meteoraDlmmAutomationWithdrawAndCloseAutomationIxPost: async (body: AutomationWithdrawAndCloseAutomationIxBody, options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
-            // verify required parameter 'body' is not null or undefined
-            if (body === null || body === undefined) {
-                throw new RequiredError('body','Required parameter body was null or undefined when calling meteoraDlmmAutomationWithdrawAndCloseAutomationIxPost.');
-            }
-            const localVarPath = `/meteora/dlmm/automation/withdrawAndCloseAutomationIx`;
-            // use dummy base URL string because the URL constructor only accepts absolute URLs.
-            const localVarUrlObj = new URL(localVarPath, 'https://example.com');
-            let baseOptions;
-            if (configuration) {
-                baseOptions = configuration.baseOptions;
-            }
-            const localVarRequestOptions :AxiosRequestConfig = { method: 'POST', ...baseOptions, ...options};
-            const localVarHeaderParameter = {} as any;
-            const localVarQueryParameter = {} as any;
-
-            localVarHeaderParameter['Content-Type'] = 'application/json';
-
-            const query = new URLSearchParams(localVarUrlObj.search);
-            for (const key in localVarQueryParameter) {
-                query.set(key, localVarQueryParameter[key]);
-            }
-            for (const key in options.params) {
-                query.set(key, options.params[key]);
-            }
-            localVarUrlObj.search = (new URLSearchParams(query)).toString();
-            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
-            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
-            const needsSerialization = (typeof body !== "string") || localVarRequestOptions.headers['Content-Type'] === 'application/json';
-            localVarRequestOptions.data =  needsSerialization ? JSON.stringify(body !== undefined ? body : {}) : (body || "");
-
-            return {
-                url: localVarUrlObj.pathname + localVarUrlObj.search + localVarUrlObj.hash,
-                options: localVarRequestOptions,
-            };
-        },
     }
 };
 
@@ -245,19 +202,6 @@ export const MeteoraDLMMAutomationInstructionsApiFp = function(configuration?: C
                 return axios.request(axiosRequestArgs);
             };
         },
-        /**
-         * Performs a full withdrawal and closes position
-         * @param {AutomationWithdrawAndCloseAutomationIxBody} body 
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        async meteoraDlmmAutomationWithdrawAndCloseAutomationIxPost(body: AutomationWithdrawAndCloseAutomationIxBody, options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => Promise<AxiosResponse<TransactionMetadata>>> {
-            const localVarAxiosArgs = await MeteoraDLMMAutomationInstructionsApiAxiosParamCreator(configuration).meteoraDlmmAutomationWithdrawAndCloseAutomationIxPost(body, options);
-            return (axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
-                const axiosRequestArgs :AxiosRequestConfig = {...localVarAxiosArgs.options, url: basePath + localVarAxiosArgs.url};
-                return axios.request(axiosRequestArgs);
-            };
-        },
     }
 };
 
@@ -293,15 +237,6 @@ export const MeteoraDLMMAutomationInstructionsApiFactory = function (configurati
          */
         async meteoraDlmmAutomationRebalanceAutomationIxPost(body: AutomationRebalanceAutomationIxBody, options?: AxiosRequestConfig): Promise<AxiosResponse<TransactionMetadata>> {
             return MeteoraDLMMAutomationInstructionsApiFp(configuration).meteoraDlmmAutomationRebalanceAutomationIxPost(body, options).then((request) => request(axios, basePath));
-        },
-        /**
-         * Performs a full withdrawal and closes position
-         * @param {AutomationWithdrawAndCloseAutomationIxBody} body 
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        async meteoraDlmmAutomationWithdrawAndCloseAutomationIxPost(body: AutomationWithdrawAndCloseAutomationIxBody, options?: AxiosRequestConfig): Promise<AxiosResponse<TransactionMetadata>> {
-            return MeteoraDLMMAutomationInstructionsApiFp(configuration).meteoraDlmmAutomationWithdrawAndCloseAutomationIxPost(body, options).then((request) => request(axios, basePath));
         },
     };
 };
@@ -342,15 +277,5 @@ export class MeteoraDLMMAutomationInstructionsApi extends BaseAPI {
      */
     public async meteoraDlmmAutomationRebalanceAutomationIxPost(body: AutomationRebalanceAutomationIxBody, options?: AxiosRequestConfig) : Promise<AxiosResponse<TransactionMetadata>> {
         return MeteoraDLMMAutomationInstructionsApiFp(this.configuration).meteoraDlmmAutomationRebalanceAutomationIxPost(body, options).then((request) => request(this.axios, this.basePath));
-    }
-    /**
-     * Performs a full withdrawal and closes position
-     * @param {AutomationWithdrawAndCloseAutomationIxBody} body 
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     * @memberof MeteoraDLMMAutomationInstructionsApi
-     */
-    public async meteoraDlmmAutomationWithdrawAndCloseAutomationIxPost(body: AutomationWithdrawAndCloseAutomationIxBody, options?: AxiosRequestConfig) : Promise<AxiosResponse<TransactionMetadata>> {
-        return MeteoraDLMMAutomationInstructionsApiFp(this.configuration).meteoraDlmmAutomationWithdrawAndCloseAutomationIxPost(body, options).then((request) => request(this.axios, this.basePath));
     }
 }
--- swagger-client/apis/meteora-dlmminstructions-api.ts
+++ swagger-client/apis/meteora-dlmminstructions-api.ts
@@ -23,6 +23,7 @@ import { TxClaimBody } from '../models';
 import { TxClosePositionBody } from '../models';
 import { TxCreatePositionAndDepositBody } from '../models';
 import { TxDepositBody } from '../models';
+import { TxInitializeBinArraysBody } from '../models';
 import { TxWithdrawBody } from '../models';
 /**
  * MeteoraDLMMInstructionsApi - axios parameter creator
@@ -200,6 +201,48 @@ export const MeteoraDLMMInstructionsApiAxiosParamCreator = function (configurati
         },
         /**
          * Return Solana instructions that create a position and deposit to Meteora via Hawksight.
+         * @param {TxInitializeBinArraysBody} body 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        meteoraDlmmTxInitializeBinArraysPost: async (body: TxInitializeBinArraysBody, options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'body' is not null or undefined
+            if (body === null || body === undefined) {
+                throw new RequiredError('body','Required parameter body was null or undefined when calling meteoraDlmmTxInitializeBinArraysPost.');
+            }
+            const localVarPath = `/meteora/dlmm/tx/initializeBinArrays`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, 'https://example.com');
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+            const localVarRequestOptions :AxiosRequestConfig = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            const query = new URLSearchParams(localVarUrlObj.search);
+            for (const key in localVarQueryParameter) {
+                query.set(key, localVarQueryParameter[key]);
+            }
+            for (const key in options.params) {
+                query.set(key, options.params[key]);
+            }
+            localVarUrlObj.search = (new URLSearchParams(query)).toString();
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            const needsSerialization = (typeof body !== "string") || localVarRequestOptions.headers['Content-Type'] === 'application/json';
+            localVarRequestOptions.data =  needsSerialization ? JSON.stringify(body !== undefined ? body : {}) : (body || "");
+
+            return {
+                url: localVarUrlObj.pathname + localVarUrlObj.search + localVarUrlObj.hash,
+                options: localVarRequestOptions,
+            };
+        },
+        /**
+         * Return Solana instructions that create a position and deposit to Meteora via Hawksight.
          * @param {TxWithdrawBody} body 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -303,6 +346,19 @@ export const MeteoraDLMMInstructionsApiFp = function(configuration?: Configurati
         },
         /**
          * Return Solana instructions that create a position and deposit to Meteora via Hawksight.
+         * @param {TxInitializeBinArraysBody} body 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async meteoraDlmmTxInitializeBinArraysPost(body: TxInitializeBinArraysBody, options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => Promise<AxiosResponse<TransactionMetadata>>> {
+            const localVarAxiosArgs = await MeteoraDLMMInstructionsApiAxiosParamCreator(configuration).meteoraDlmmTxInitializeBinArraysPost(body, options);
+            return (axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
+                const axiosRequestArgs :AxiosRequestConfig = {...localVarAxiosArgs.options, url: basePath + localVarAxiosArgs.url};
+                return axios.request(axiosRequestArgs);
+            };
+        },
+        /**
+         * Return Solana instructions that create a position and deposit to Meteora via Hawksight.
          * @param {TxWithdrawBody} body 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
@@ -358,6 +414,15 @@ export const MeteoraDLMMInstructionsApiFactory = function (configuration?: Confi
          */
         async meteoraDlmmTxDepositPost(body: TxDepositBody, options?: AxiosRequestConfig): Promise<AxiosResponse<TransactionMetadata>> {
             return MeteoraDLMMInstructionsApiFp(configuration).meteoraDlmmTxDepositPost(body, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * Return Solana instructions that create a position and deposit to Meteora via Hawksight.
+         * @param {TxInitializeBinArraysBody} body 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async meteoraDlmmTxInitializeBinArraysPost(body: TxInitializeBinArraysBody, options?: AxiosRequestConfig): Promise<AxiosResponse<TransactionMetadata>> {
+            return MeteoraDLMMInstructionsApiFp(configuration).meteoraDlmmTxInitializeBinArraysPost(body, options).then((request) => request(axios, basePath));
         },
         /**
          * Return Solana instructions that create a position and deposit to Meteora via Hawksight.
@@ -417,6 +482,16 @@ export class MeteoraDLMMInstructionsApi extends BaseAPI {
      */
     public async meteoraDlmmTxDepositPost(body: TxDepositBody, options?: AxiosRequestConfig) : Promise<AxiosResponse<TransactionMetadata>> {
         return MeteoraDLMMInstructionsApiFp(this.configuration).meteoraDlmmTxDepositPost(body, options).then((request) => request(this.axios, this.basePath));
+    }
+    /**
+     * Return Solana instructions that create a position and deposit to Meteora via Hawksight.
+     * @param {TxInitializeBinArraysBody} body 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof MeteoraDLMMInstructionsApi
+     */
+    public async meteoraDlmmTxInitializeBinArraysPost(body: TxInitializeBinArraysBody, options?: AxiosRequestConfig) : Promise<AxiosResponse<TransactionMetadata>> {
+        return MeteoraDLMMInstructionsApiFp(this.configuration).meteoraDlmmTxInitializeBinArraysPost(body, options).then((request) => request(this.axios, this.basePath));
     }
     /**
      * Return Solana instructions that create a position and deposit to Meteora via Hawksight.
