0e926f39edf7cc3fa92e7675a760e6cbc3116d1a
ツカサちゃん <11975597+ghabxph@users.noreply.github.com>
2024-08-12 12:15:51 +0800

    feat(#10): removed orca dependency

--- hawk-api/src/anchor.ts
+++ hawk-api/src/anchor.ts
@@ -2,13 +2,13 @@ import * as web3 from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
 import { IDL as _IyfMain, IndexYieldFarming } from "./idl/iyf-main-idl";
 import { IDL as _IyfExtension, IyfExtension } from "./idl/iyf-extension-idl";
+import { IDL as _Whirlpool, Whirlpool } from "./idl/orca-idl";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 import {
   IYF_EXTENSION,
   IYF_MAIN,
   ORCA_WHIRLPOOL_PROGRAM
 } from "./addresses";
-import { WhirlpoolContext } from "@orca-so/whirlpools-sdk";
 
 export class Anchor {
 
@@ -16,7 +16,7 @@ export class Anchor {
   provider: anchor.AnchorProvider;
   iyfMain: anchor.Program<IndexYieldFarming>;
   iyfExtension: anchor.Program<IyfExtension>;
-  whirlpoolCtx: WhirlpoolContext
+  orcaProgram: anchor.Program<Whirlpool>;
   
   private constructor(
     public connection: web3.Connection,
@@ -24,7 +24,7 @@ export class Anchor {
     this.provider = new anchor.AnchorProvider(connection, new NodeWallet(web3.Keypair.generate()), {});
     this.iyfMain = new anchor.Program(_IyfMain, IYF_MAIN, this.provider);
     this.iyfExtension =  new anchor.Program(_IyfExtension, IYF_EXTENSION, this.provider);
-    this.whirlpoolCtx = WhirlpoolContext.withProvider(this.provider, ORCA_WHIRLPOOL_PROGRAM);
+    this.orcaProgram = new anchor.Program(_Whirlpool, ORCA_WHIRLPOOL_PROGRAM, this.provider);
   }
 
   static initialize(connection: web3.Connection) {
