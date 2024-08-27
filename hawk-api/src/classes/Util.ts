2ad5ca0cfd2d46c97a2e18f2277a440b7e4f8f23
ツカサちゃん <11975597+ghabxph@users.noreply.github.com>
2024-08-27 17:55:20 +0800

    feat(#13): fix typo

--- hawk-api/src/classes/Util.ts
+++ hawk-api/src/classes/Util.ts
@@ -1,5 +1,5 @@
 import * as client from "@hawksightco/swagger-client";
-import { MeteoraDlmmActiveBin, ResponseWithStatus, UserPortfolio } from "../types";
+import { MeteoraDlmmActiveBin, ResponseWithStatus, UserPortfolio, UserPortfolioOut } from "../types";
 import { Client } from "./Client";
 
 /**
@@ -40,7 +40,7 @@ export class Util {
       wallet: string,
       pool?: string,
     }
-  ): Promise<ResponseWithStatus<UserPortfolio>> {
+  ): Promise<ResponseWithStatus<UserPortfolioOut>> {
     const result = await this.client.meteoraDLMMUtilityFunctionsApi.meteoraDlmmUtilPositionsGet(params.wallet, params.pool).catch(e => e.response);
     return {
       status: result.status,
