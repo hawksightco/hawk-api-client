096c7e3e3855bd415a90da0d20af976f1d427e0a
ガビーくん <11975597+ghabxph@users.noreply.github.com>
2024-10-21 15:13:43 +0800

    test: debug

--- hawk-api/test/index.spec.ts
+++ hawk-api/test/index.spec.ts
@@ -116,63 +116,63 @@ describe('Meteora Endpoints', () => {
     // await simulateOrExecuteTransaction(result.data);
     await localnetTest(result, [testWalletKp, positionKp]);
   }, TIMEOUT);
-//   it ('POST /meteora/dlmm/tx/deposit', async () => {
-//     const result = await client.txGenerator.meteoraDeposit(
-//       connection,
-//       testWallet.toString(),
-//       {
-//         position: testPosition,
-//         userWallet: testWallet.toString(),
-//         totalXAmount: 10_000,
-//         totalYAmount: 10_000,
-//         distribution: 'CURVE',
-//       }
-//     );
-//     logIfNot200(result);
-//     expect(result.status).toBe(200);
-//     await simulateOrExecuteTransaction(result.data);
-//   }, TIMEOUT);
-//   it ('POST /meteora/dlmm/tx/claim', async () => {
-//     const result = await client.txGenerator.meteoraClaim(
-//       connection,
-//       testWallet.toString(),
-//       {
-//         position: testPosition,
-//         userWallet: testWallet.toString(),
-//       }
-//     );
-//     logIfNot200(result);
-//     expect(result.status).toBe(200);
-//     await simulateOrExecuteTransaction(result.data);
-//   }, TIMEOUT);
-//   it ('POST /meteora/dlmm/tx/withdraw', async () => {
-//     const result = await client.txGenerator.meteoraWithdraw(
-//       connection,
-//       testWallet.toString(),
-//       {
-//         position: testPosition,
-//         userWallet: testWallet.toString(),
-//         amountBps: 10_000,
-//         shouldClaimAndClose: true,
-//       }
-//     );
-//     logIfNot200(result);
-//     expect(result.status).toBe(200);
-//     await simulateOrExecuteTransaction(result.data);
-//   }, TIMEOUT);
-//   it ('POST /meteora/dlmm/tx/closePosition', async () => { // will not work because position is not empty.
-//     const result = await client.txGenerator.meteoraClosePosition(
-//       connection,
-//       testWallet.toString(),
-//       {
-//         position: testPosition,
-//         userWallet: testWallet.toString(),
-//       }
-//     );
-//     logIfNot200(result);
-//     expect(result.status).toBe(200);
-//     await simulateOrExecuteTransaction(result.data);
-//   }, TIMEOUT);
+  // it ('POST /meteora/dlmm/tx/deposit', async () => {
+  //   const result = await client.txGenerator.meteoraDeposit(
+  //     connection,
+  //     testWallet.toString(),
+  //     {
+  //       position: testPosition,
+  //       userWallet: testWallet.toString(),
+  //       totalXAmount: 10_000,
+  //       totalYAmount: 10_000,
+  //       distribution: 'CURVE',
+  //     }
+  //   );
+  //   logIfNot200(result);
+  //   expect(result.status).toBe(200);
+  //   await simulateOrExecuteTransaction(result.data);
+  // }, TIMEOUT);
+// //   it ('POST /meteora/dlmm/tx/claim', async () => {
+// //     const result = await client.txGenerator.meteoraClaim(
+// //       connection,
+// //       testWallet.toString(),
+// //       {
+// //         position: testPosition,
+// //         userWallet: testWallet.toString(),
+// //       }
+// //     );
+// //     logIfNot200(result);
+// //     expect(result.status).toBe(200);
+// //     await simulateOrExecuteTransaction(result.data);
+// //   }, TIMEOUT);
+// //   it ('POST /meteora/dlmm/tx/withdraw', async () => {
+// //     const result = await client.txGenerator.meteoraWithdraw(
+// //       connection,
+// //       testWallet.toString(),
+// //       {
+// //         position: testPosition,
+// //         userWallet: testWallet.toString(),
+// //         amountBps: 10_000,
+// //         shouldClaimAndClose: true,
+// //       }
+// //     );
+// //     logIfNot200(result);
+// //     expect(result.status).toBe(200);
+// //     await simulateOrExecuteTransaction(result.data);
+// //   }, TIMEOUT);
+  // it ('POST /meteora/dlmm/tx/closePosition', async () => { // will not work because position is not empty.
+  //   const result = await client.txGenerator.meteoraClosePosition(
+  //     connection,
+  //     testWallet.toString(),
+  //     {
+  //       position: testPosition,
+  //       userWallet: testWallet.toString(),
+  //     }
+  //   );
+  //   logIfNot200(result);
+  //   expect(result.status).toBe(200);
+  //   await simulateOrExecuteTransaction(result.data);
+  // }, TIMEOUT);
 });
 
 // // describe('Meteora Automation Endpoints', () => {
@@ -221,95 +221,100 @@ describe('Meteora Endpoints', () => {
 // //   }, TIMEOUT);
 // // });
 
-// describe('Orca Transaction Generation', () => {
-//   it ('Orca Open Position', async () => {
-//     Anchor.initialize(connection);
-//     const pool = await Anchor.instance().orcaProgram.account.whirlpool.fetch(testOrcaPool);
-//     const tickLowerIndex = Math.floor((pool.tickCurrentIndex - 50 * pool.tickSpacing) / pool.tickSpacing) * pool.tickSpacing;
-//     const tickUpperIndex = Math.floor((pool.tickCurrentIndex + 50 * pool.tickSpacing) / pool.tickSpacing) * pool.tickSpacing;
-//     const result = await client.txGenerator.orcaOpenPosition(
-//       connection,
-//       testWallet.toString(),
-//       {
-//         userWallet: testWallet,
-//         positionMint: positionMint,
-//         whirlpool: testOrcaPool,
-//         tickLowerIndex,
-//         tickUpperIndex,
-//       }
-//     );
-//     logIfNot200(result);
-//     expect(result.status).toBe(200);
-//     // await simulateOrExecuteTransaction(result.data, [testWalletKp, positionMintKp]);
-//     await localnetTest(result, [testWalletKp, positionMintKp]);
-//   }, TIMEOUT);
+describe('Orca Transaction Generation', () => {
+  // it ('Orca Open Position', async () => {
+  //   Anchor.initialize(connection);
+  //   const pool = await Anchor.instance().orcaProgram.account.whirlpool.fetch(testOrcaPool);
+  //   console.log(`pool.tickCurrentIndex: ${pool.tickCurrentIndex}`);
+  //   const tickLowerIndex = Math.floor((pool.tickCurrentIndex - 50 * pool.tickSpacing) / pool.tickSpacing) * pool.tickSpacing;
+  //   const tickUpperIndex = Math.floor((pool.tickCurrentIndex + 50 * pool.tickSpacing) / pool.tickSpacing) * pool.tickSpacing;
+  //   const result = await client.txGenerator.orcaOpenPosition(
+  //     connection,
+  //     testWallet.toString(),
+  //     {
+  //       userWallet: testWallet,
+  //       positionMint: positionMint,
+  //       whirlpool: testOrcaPool,
+  //       tickLowerIndex,
+  //       tickUpperIndex,
+  //     }
+  //   );
+  //   logIfNot200(result);
+  //   expect(result.status).toBe(200);
+  //   // await simulateOrExecuteTransaction(result.data, [testWalletKp, positionMintKp]);
+  //   await localnetTest(result, [testWalletKp, positionMintKp], [], false);
+  // }, TIMEOUT);
 
-// //   it ('Orca Close Position', async () => {
-// //     const result = await client.txGenerator.orcaClosePosition(
-// //       connection,
-// //       testWallet.toString(),
-// //       {
-// //         userWallet: testWallet,
-// //         positionMint,
-// //       }
-// //     );
-// //     logIfNot200(result);
-// //     expect(result.status).toBe(200);
-// //     await simulateOrExecuteTransaction(result.data, [testWalletKp]);
-// //   }, TIMEOUT);
+  // it ('Orca Close Position', async () => {
+  //   const result = await client.txGenerator.orcaClosePosition(
+  //     connection,
+  //     testWallet.toString(),
+  //     {
+  //       userWallet: testWallet,
+  //       positionMint,
+  //     }
+  //   );
+  //   logIfNot200(result);
+  //   expect(result.status).toBe(200);
+  //   // await simulateOrExecuteTransaction(result.data, [testWalletKp]);
+  //   await localnetTest(result, [testWalletKp, positionMintKp]);
+  // }, TIMEOUT);
 
-// //   it ('Orca Deposit', async () => {
-// //     const position = generateOrcaPositionPDA(testPositionMint);
-// //     let positionInfo = await Anchor.instance().orcaProgram.account.position.fetch(position);
-// //     const currentLiquidity = positionInfo.liquidity;
-// //     console.log(`position: ${position}`);
-// //     const result = await client.txGenerator.orcaDeposit(
-// //       connection,
-// //       testWallet.toString(),
-// //       {
-// //         userWallet: testWallet,
-// //         positionMint: testPositionMint,
-// //         totalXAmount: new BN('10000'),
-// //         totalYAmount: new BN('10000')
-// //       }
-// //     );
-// //     logIfNot200(result);
-// //     expect(result.status).toBe(200);
-// //     await simulateOrExecuteTransaction(result.data, [testWalletKp]);
-// //     positionInfo = await Anchor.instance().orcaProgram.account.position.fetch(position);
-// //     liquidityDelta = positionInfo.liquidity.sub(currentLiquidity);
-// //   }, TIMEOUT);
+  // it ('Orca Deposit', async () => {
+  //   const position = generateOrcaPositionPDA(testPositionMint);
+  //   let positionInfo = await Anchor.instance().orcaProgram.account.position.fetch(position);
+  //   const currentLiquidity = positionInfo.liquidity;
+  //   console.log(`position: ${position}`);
+  //   const result = await client.txGenerator.orcaDeposit(
+  //     connection,
+  //     testWallet.toString(),
+  //     {
+  //       userWallet: testWallet,
+  //       positionMint: testPositionMint,
+  //       totalXAmount: new BN('10000'),
+  //       totalYAmount: new BN('10000')
+  //     }
+  //   );
+  //   logIfNot200(result);
+  //   expect(result.status).toBe(200);
+  //   // await simulateOrExecuteTransaction(result.data, [testWalletKp]);
+  //   await localnetTest(result, [testWalletKp, positionMintKp]);
+  //   positionInfo = await Anchor.instance().orcaProgram.account.position.fetch(position);
+  //   liquidityDelta = positionInfo.liquidity.sub(currentLiquidity);
+  // }, TIMEOUT);
 
-// //   it ('Orca Claim Rewards', async () => {
-// //     const result = await client.txGenerator.orcaClaimRewards(
-// //       connection,
-// //       testWallet.toString(),
-// //       {
-// //         userWallet: testWallet,
-// //         positionMint: testPositionMint,
-// //       }
-// //     );
-// //     logIfNot200(result);
-// //     expect(result.status).toBe(200);
-// //     await simulateOrExecuteTransaction(result.data, [testWalletKp]);
-// //   }, TIMEOUT);
+  // it ('Orca Claim Rewards', async () => {
+  //   const result = await client.txGenerator.orcaClaimRewards(
+  //     connection,
+  //     testWallet.toString(),
+  //     {
+  //       userWallet: testWallet,
+  //       positionMint: testPositionMint,
+  //     }
+  //   );
+  //   logIfNot200(result);
+  //   expect(result.status).toBe(200);
+  //   // await simulateOrExecuteTransaction(result.data, [testWalletKp]);
+  //   await localnetTest(result, [testWalletKp]);
+  // }, TIMEOUT);
 
-// //   it ('Orca Withdraw', async () => {
-// //     console.log(`liquidityDelta: ${liquidityDelta}`);
-// //     const result = await client.txGenerator.orcaWithdraw(
-// //       connection,
-// //       testWallet.toString(),
-// //       {
-// //         userWallet: testWallet,
-// //         positionMint: testPositionMint,
-// //         liquidityAmount: liquidityDelta,
-// //       }
-// //     );
-// //     logIfNot200(result);
-// //     expect(result.status).toBe(200);
-// //     await simulateOrExecuteTransaction(result.data, [testWalletKp]);
-// //   }, TIMEOUT);
-// });
+  // it ('Orca Withdraw', async () => {
+  //   console.log(`liquidityDelta: ${liquidityDelta}`);
+  //   const result = await client.txGenerator.orcaWithdraw(
+  //     connection,
+  //     testWallet.toString(),
+  //     {
+  //       userWallet: testWallet,
+  //       positionMint: testPositionMint,
+  //       liquidityAmount: liquidityDelta,
+  //     }
+  //   );
+  //   logIfNot200(result);
+  //   expect(result.status).toBe(200);
+  //   // await simulateOrExecuteTransaction(result.data, [testWalletKp]);
+  //   await localnetTest(result, [testWalletKp]);
+  // }, TIMEOUT);
+});
 
 function logIfNot200(result: ResponseWithStatus<any>) {
   if (result.status !== 200) {
@@ -328,19 +333,19 @@ async function simulateOrExecuteTransaction(txMetadata: TransactionMetadata, sig
   console.log(txMetadata.description);
   console.log(`-----------------------------------------`);
   const simulation = await txMetadata.transaction.simulateTransaction(connection, signers);
+  for (const log of simulation.logs as string[]) {
+    console.log(log);
+  }
+  console.log(`Consumed ${simulation.unitsConsumed} units`);
+  console.log(``)
+  console.log(``)
+  console.log(``)
   if (simulation.err) {
-    for (const log of simulation.logs as string[]) {
-      console.log(log);
-    }
-    console.log(`Consumed ${simulation.unitsConsumed} units`);
-    console.log(``)
-    console.log(``)
-    console.log(``)
     throw new Error(`Simulation has error`);
   }
   const executeTransaction = (process.env.EXECUTE_TRANSACTION as string).toLowerCase() === 'true';
   if (executeTransaction && signers.length > 0) {
-    // txMetadata.transaction.addPriorityFeeIx(connection, Math.ceil(simulation.unitsConsumed * 1.1), false, PriorityLevel.High);
+    await txMetadata.transaction.addPriorityFeeIx(connection, Math.ceil(simulation.unitsConsumed * 1.1), false, PriorityLevel.High);
     txMetadata.transaction.sign(signers);
     const rawTx = txMetadata.transaction.versionedTransaction.serialize();
     const signature = await connection.sendRawTransaction(rawTx);
@@ -356,13 +361,13 @@ async function simulateOrExecuteTransaction(txMetadata: TransactionMetadata, sig
   return simulation;
 }
 
-async function localnetTest(tx: ResponseWithStatus<TransactionMetadata>, signers: web3.Keypair[], requestAirdrop: web3.PublicKey[] = []) {
+async function localnetTest(tx: ResponseWithStatus<TransactionMetadata>, signers: web3.Keypair[], requestAirdrop: web3.PublicKey[] = [], kill: boolean = true) {
   const testValidator = new SolanaTestValidator();
   testValidator.rpc = process.env.RPC_URL as string;
   testValidator.path = process.env.TEST_VALIDATOR_PATH as string;
-  testValidator.addProgram('FqGg2Y1FNxMiGd51Q6UETixQWkF5fB92MysbYogRJb3P', `/${process.env.PATH_index_yield_farming}`);
-  testValidator.addProgram('6qZ4eVTH5ZbcK7gaqKNUSg7dLyCxTGZB43G4NcQdP2LZ', `/${process.env.PATH_iyf_strategy_quarry}`);
-  testValidator.addProgram('8WViAZqpgDWxV2VxiQ2RZ8WRXqFieDnrJsspE3DtWnpn', `/${process.env.PATH_iyf_strategy_solend}`);
+  // testValidator.addProgram('FqGg2Y1FNxMiGd51Q6UETixQWkF5fB92MysbYogRJb3P', `/${process.env.PATH_index_yield_farming}`);
+  // testValidator.addProgram('6qZ4eVTH5ZbcK7gaqKNUSg7dLyCxTGZB43G4NcQdP2LZ', `/${process.env.PATH_iyf_strategy_quarry}`);
+  // testValidator.addProgram('8WViAZqpgDWxV2VxiQ2RZ8WRXqFieDnrJsspE3DtWnpn', `/${process.env.PATH_iyf_strategy_solend}`);
   testValidator.addProgram('EZiUb6ydWpR3ciizBTJ1J36KCqLyPKVjh4yZEJbs5Uno', `/${process.env.PATH_iyf_extension}`);
   console.log(`testWallet: ${testWallet}`);
   console.log(`tx.data.transaction.payerKey: ${tx.data.transaction.payerKey}`);
@@ -378,7 +383,7 @@ async function localnetTest(tx: ResponseWithStatus<TransactionMetadata>, signers
     payer: tx.data.transaction.payerKey,
     addressLookupTable: tx.data.transaction.alts.map(alt => alt.key),
     signers,
-  }], requestAirdrop);
+  }], requestAirdrop, kill);
 }
 
 function decodeIxName(data: Buffer) {
