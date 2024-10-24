bbda5a55e30a483c8a4b545ed31910ded6e228db
ガビーくん <11975597+ghabxph@users.noreply.github.com>
2024-10-24 11:31:00 +0800

    feat: added more assert to make sure the appended instructions are correct

--- hawk-api/test/classes/Atomicity.spec.ts
+++ hawk-api/test/classes/Atomicity.spec.ts
@@ -2,6 +2,7 @@ import * as web3 from "@solana/web3.js";
 import { Atomicity } from "../../src/classes/Atomicity";
 import { TransactionInstruction } from "@solana/web3.js";
 import { SimpleIxGenerator } from "../../src/classes/SimpleIxGenerator";
+import { sighashMatch } from "../../src/functions";
 
 describe('Atomicity', () => {
   it('Be able to create new instance of Atomicity without error', async () => {
@@ -44,12 +45,42 @@ describe('Atomicity', () => {
     // Build batch
     const batch = await atomicity.buildBatch();
 
-    // Expect 5 batch of transaction from given 100 instruction
+    // Expect 6 batch of transaction from given 100 instruction
     expect(batch.length).toBe(6);
 
     // Expect 106 instructions from batch
     let count = 0;
     batch.map(ixs => { count += ixs.length });
-    expect(count).toBe(106)
+    expect(count).toBe(106);
+
+    // Expect first batch to have `setTransactionSlot`
+    const firstIxData = batch[0][batch[0].length - 1].data;
+    expect(firstIxData.length).toBeGreaterThanOrEqual(8);
+    expect(sighashMatch(firstIxData, "setTransactionSlot")).toBe(true);
+
+    // Expect second batch to have `verifyTransactionSlot`
+    const secondIxData = batch[1][batch[1].length - 1].data;
+    expect(secondIxData.length).toBeGreaterThanOrEqual(8);
+    expect(sighashMatch(secondIxData, "verifyTransactionSlot")).toBe(true);
+
+    // Expect third batch to have `verifyTransactionSlot`
+    const thirdIxData = batch[2][batch[2].length - 1].data;
+    expect(thirdIxData.length).toBeGreaterThanOrEqual(8);
+    expect(sighashMatch(thirdIxData, "verifyTransactionSlot")).toBe(true);
+
+    // Expect fourth batch to have `verifyTransactionSlot`
+    const fourthIxData = batch[3][batch[3].length - 1].data;
+    expect(fourthIxData.length).toBeGreaterThanOrEqual(8);
+    expect(sighashMatch(fourthIxData, "verifyTransactionSlot")).toBe(true);
+
+    // Expect fifth batch to have `verifyTransactionSlot`
+    const fifthIxData = batch[4][batch[4].length - 1].data;
+    expect(fifthIxData.length).toBeGreaterThanOrEqual(8);
+    expect(sighashMatch(fifthIxData, "verifyTransactionSlot")).toBe(true);
+
+    // Expect sixth batch to have `verifyTransactionSlot`
+    const sixthIxData = batch[5][batch[5].length - 1].data;
+    expect(sixthIxData.length).toBeGreaterThanOrEqual(8);
+    expect(sighashMatch(sixthIxData, "verifyTransactionSlot")).toBe(true);
   });
 });
